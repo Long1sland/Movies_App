1966d8fd117a581e85b49fd4fc6553fda0c98e12
Feyisayo Ibukun <ibukun209@gmail.com>
2021-12-08 17:51:27 +0100

    dark mode implimented

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -23,6 +23,9 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    @media (prefers-color-scheme: dark){
+      background-color: black;
+    }
 
     h1 {
       font-size: 2rem;
--- src/components/Grid/Grid.styles.js
+++ src/components/Grid/Grid.styles.js
@@ -5,9 +5,16 @@ export const Wrapper = styled.div`
   max-width: var(--maxWidth);
   margin: 0 auto;
   padding: 0 20px;
+  
+  
 
   h1 {
     color: var(--medGrey);
+
+    @media (prefers-color-scheme: dark) {
+    color:white;
+  }
+  
   }
 
   h1 {
